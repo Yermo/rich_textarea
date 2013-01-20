91813b10e4c958e98fb4b70f0f6d0160a92ad66a
Yermo <yml@yml.com>
2013-01-20 17:39:14 -0500

    more typos

--- ddt.js
+++ ddt.js
@@ -1,6 +1,6 @@
 
 /**
-* Copyright (c) 2013 DTLink, LLC (http://www.asoftwareguy.com)
+* Copyright (c) 2013 DTLink, LLC (http://www.a-software-guy.com)
 *
 * Permission  is  hereby  granted,  free of charge, to any person
 * obtaining  a copy of this software and associated documentation
